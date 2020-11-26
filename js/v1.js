81fb5e60b44d7d081715af93df0785754037ca71
Raphael04-lo <losko.r04@htlwienwest.at>
2020-11-26 08:08:19 +0100

    commit

--- js/v1.js
+++ js/v1.js
@@ -1,10 +1,11 @@
 const numberCards = 16
-let openCards = []
+let openedCards = []
 
 let p = document.querySelector('.deck')
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
         c.innerHTML = index + 1
+        c.type = index % 2 + 1
         c.className="card"
         c.addEventListener('click',flipp)
         p.appendChild(c)
@@ -13,8 +14,8 @@ for (let index = 0; index < numberCards; index++) {
 function flipp(event){
     if(!this.classList.contains('found'))
         openCard(this)
-    //this.classList.toggle('flip')
+    this.classList.toggle('flipped')
 }
 function openCard(c){
-    alert("open")
+    alert(c.innerHTML)
 }
