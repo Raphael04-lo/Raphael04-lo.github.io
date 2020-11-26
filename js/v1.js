9fbd37b2813c249779023174140ee2fc4876d4fb
Raphael04-lo <losko.r04@htlwienwest.at>
2020-11-26 08:45:06 +0100

    Memory_shuffle

--- js/v1.js
+++ js/v1.js
@@ -1,15 +1,31 @@
+/*
+1) Leeres Array zum Speichern der aufgedeckten Karten (openedCards)
+2) Karten richtig erzeugen (type) [1,1,2,2,3,3,4,4.........8,8]
+3)KArten mischen (shuffle)
+4) Karten zum Deck hinzufügen
+5) Klick Eventhandler
+6)Kartenlogik:
+    6.1) prüfen wie viele Karten bereits aufgedeckt sind
+    6.2) wenn weniger als 2 -> aufdecken
+    6.3)Erkennen ob zwei gleiche Karten
+    aufgedeckt wurden -> wenn gleich -> found; wenn nicht gleich -> verdecken
+*/
+
 const numberCards = 16
+let openCards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 let openedCards = []
+shuffle(openCards)
 
 let p = document.querySelector('.deck')
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
-        c.innerHTML = index + 1
-        c.type = index % 2 + 1
+        c.innerHTML = openCards[index]
+        c.type = openCards[index]
         c.className="card"
         c.addEventListener('click',flipp)
         p.appendChild(c)
 }
+
 
 function flipp(event){
     if(!this.classList.contains('found'))
@@ -19,3 +35,19 @@ function flipp(event){
 function openCard(c){
     alert(c.innerHTML)
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
